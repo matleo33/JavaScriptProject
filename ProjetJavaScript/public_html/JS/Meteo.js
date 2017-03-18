200bef5f83e53cad2503929fb7a4615abb6330ef
yberwit <yberwit@gmail.com>
2017-03-18 21:16:15 +0100

    Widget meteo avec bug addEventListener is null !

--- ProjetJavaScript/public_html/JS/Meteo.js
+++ ProjetJavaScript/public_html/JS/Meteo.js
@@ -3,5 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
+document.getElementById('buttonMeteo').addEventListener('click',function () {
+    var page = "http://api.openweathermap.org/data/2.5/weather?q="+document.getElementById('ville').value+"&APPID=baba13f455b4b825452802461eabb87a";
+    var requeteAjax = new XMLHttpRequest();
+    if (requeteAjax != null) {
+        requeteAjax.open("GET", page, true);
+        requeteAjax.addEventListener('readystatechange',function () {
+            if (requeteAjax.readyState === XMLHttpRequest.DONE) {
+                if (requeteAjax.status === 200) {
+                    var json = JSON.parse(requeteAjax.responseText);
+                    var text = 'Ville :'+ json.name
+                        + '\n Temps :' + json.weather[0].description
+                        + '\n Tempèrature :'+ (parseInt(json.main.temp)-273.15).toString().substring(0,5)+'°C'
+                        + "\n Max :" + (parseInt(json.main.temp_max)-273.15).toString().substring(0,5)+'°C' + ' Min :'+(parseInt(json.main.temp_min)-273.15).toString().substring(0,5)+'°C'
+                        + "\n Humidité :"+json.main.humidity + '%';
+                    document.getElementById('result').innerText=text;
+                }
+            }
+        });
+        requeteAjax.send();
+    }
+    else
+    {
+        window.alert("Pas de support AJAX");
+    }
+});
