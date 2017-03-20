4c138d4204ca2c503208d3464c75ebfad3cf8b63
Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2017-03-20 17:15:16 +0100

    coucou

--- ProjetJavaScript/public_html/JS/Photo.js
+++ ProjetJavaScript/public_html/JS/Photo.js
@@ -4,41 +4,17 @@
  * and open the template in the editor.
  */
 
-/*function AfficherPhoto() {
-    var URL = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"+document.getElementById('recherchePhoto').value;
+function appelPhoto() {
+    var URL = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
     $.getJSON(URL,{
         tags: $("#searchPhoto").val(),
         tagmode: "any",
         format : "json"}).done(function(reponse)
     {
-        var image = reponse.items[0].media;
+        var image = reponse.items[0].media.m;
         var img = document.createElement('img');
         img.src = image;
         document.getElementById('photo').appendChild(img);
 
     });
-}*/
-
-function appelPhoto()
-{
-    var requeteAjax = new XMLHttpRequest();
-    if (requeteAjax != null) {
-        requeteAjax.open("GET","https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"+document.getElementById('recherchePhoto').value, true);
-        requeteAjax.addEventListener('readystatechange',function () {
-            if (requeteAjax.readyState === XMLHttpRequest.DONE) {
-                if (requeteAjax.status === 200) {
-                    var json = requeteAjax.responseText;
-                    var image = json.items[0].media;
-                    var img = document.createElement('img');
-                    img.src = image;
-                    document.getElementById('photo').appendChild(img);
-                }
-            }
-        });
-        requeteAjax.send();
-    }
-    else
-    {
-        window.alert("Pas de support AJAX");
-    }
 }
