a9a5b1d3a33cd13c4ec862278927cc7a84aa2972
Mathieu <mathieu.leothaud@etu.u-bordeaux.fr>
2017-03-20 14:28:25 +0100

    vv

--- ProjetJavaScript/public_html/JS/Youtube.js
+++ ProjetJavaScript/public_html/JS/Youtube.js
@@ -5,6 +5,12 @@
  */
 
 
-function afficherYoutube() {
-    
+function rechercheYoutube() {
+    var recherche = document.getElementById('recherche').value;
+    var scr = "<iframe id=\"ytplayer\" src=\"http://www.youtube.com/embed?listType=search&list="+recherche+"/>";
+    var script = document.createElement('script');
+    script.src = scr;
+    script.defer = true;
+    script.async = true;
+    document.getElementsByTagName('head')[0].appendChild(script);
 }
